afbf0ef62a2ba6b685d42d803323eed033ba148f
Vincent Honca <vincenth1985@gmail.com>
2019-06-07 09:08:26 +0200

    finish with class musiccollection and Playlist

--- src/mymusic/MusicCollection.java
+++ src/mymusic/MusicCollection.java
@@ -3,12 +3,30 @@ package mymusic;
 //Verder is er het MusicCollection object, deze bevat een collectie van playlists.
 
 
-
-
+import java.util.Arrays;
 
 public class MusicCollection {
 
 
+    private Playlist[] playlists;
+
+    public MusicCollection(Playlist[] playlists) {
+        this.playlists = playlists;
+    }
 
 
+    public Playlist[] getPlaylists() {
+        return playlists;
+    }
+
+    public void setPlaylists(Playlist[] playlists) {
+        this.playlists = playlists;
+    }
+
+    @Override
+    public String toString() {
+        return "MusicCollection{" +
+                "playlists=" + Arrays.toString(playlists) +
+                '}';
+    }
 }
--- src/mymusic/Playlist.java
+++ src/mymusic/Playlist.java
@@ -2,5 +2,31 @@ package mymusic;
 
 //Een PlayList object daarentegen bevat een collectie van songs.
 
+import java.util.Arrays;
+
 public class Playlist {
+
+    private Song[] songs;
+
+
+    public Playlist(Song[] songs) {
+        this.songs = songs;
+    }
+
+
+    public Song[] getSongs() {
+        return songs;
+    }
+
+    public void setSongs(Song[] songs) {
+        this.songs = songs;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Playlist{" +
+                "songs=" + Arrays.toString(songs) +
+                '}';
+    }
 }
